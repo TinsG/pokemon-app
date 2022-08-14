cf72fc0b185ee35719082e95b127618d45af42d4
Tinsae Getachew <tinsugecho@gmail.com>
2022-08-14 04:40:17 +0300

    minor fix on pokmon nick name form

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -11,8 +11,8 @@ const Pokemon: React.FC<{}> = () => {
   const { pokemon: pokemonName } = useParams();
   const pokemon = useFindPokemon(pokemonName);
   const { addPokemon } = useMyPokemon();
-  
-  
+
+
   const [isOpen, setisOpen] = useState(false);
   const [nickName, setnickName] = useState('');
 
@@ -59,16 +59,17 @@ const Pokemon: React.FC<{}> = () => {
 
             </div>
             {isOpen ?
-              <div className="w-full md:w-1/3 justify-items-end ">
-                <div className="mb-6">
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nick Name</label>
-                  <input type="text" id="text" value={nickName} onChange={(e) => setnickName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+              <form onSubmit={handleAddProduct}>
+                <div className="w-full md:w-1/3 justify-items-end ">
+                  <div className="mb-6">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nick Name</label>
+                    <input type="text" id="text" value={nickName} onChange={(e) => setnickName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Nick Name" required />
+                  </div>
+
+                  <button type="submit" disabled={nickName===''} onClick={_ => handleAddProduct()} className="text-yellow-200 my-2 md:mx-5 bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                  <button type="button" onClick={_ => closeCatchForm()} className="text-white my-2 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
                 </div>
-
-
-                <button type="submit" onClick={_ => handleAddProduct()} className="text-white my-2 md:mx-5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                <button type="submit" onClick={_ => closeCatchForm()} className="text-white my-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</button>
-              </div>
+              </form>
               : <></>}
 
             <PokemonCard name={pokemon.data.data.name} showStats />
